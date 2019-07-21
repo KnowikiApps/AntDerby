3c973cb18a2145b445a7acc51ecfdbbe279cb66b
Derek Nowicki <derek@knowikiapps.com>
2019-07-20 18:00:33 -0700

    refactor app root to use class component for home page

---
+++ src/AntPage/AntPage.js
@@ -0,0 +1,52 @@
+import React, { Component, Fragment } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+class AntPage extends Component {
+  render() {
+    return (
+      <Fragment>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}
+          >
+            <View style={styles.body}>
+              <Text>stuff and things</Text>
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </Fragment>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: 'white',
+  },
+  body: {
+    backgroundColor: 'white',
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: 'white',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default AntPage;
