d316fe045a133c7a329b1375ffee3c1f0096db0b
Derek Nowicki <derek@knowikiapps.com>
2019-07-22 17:23:25 -0700

    functional component for ant stats list item component

---
+++ src/AntStats/AntStats.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+function AntStats(props) {
+  return <Text>{props.name}</Text>;
+}
+
+const styles = StyleSheet.create({});
+
+export default AntStats;
