f5000e5a9235190149b9c6404828befdf91130e9
Derek Nowicki <derek@knowikiapps.com>
2019-07-22 18:32:23 -0700

    refactor component for row layout

--- src/AntStats/AntStats.js
+++ src/AntStats/AntStats.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 function AntStats(props) {
-  return <Text>{props.name}</Text>;
+  return (
+    <View style={styles.rowLayout}>
+      <View style={styles.column1} />
+      <View style={styles.column2}>
+        <Text>{props.name}</Text>
+      </View>
+      <View style={styles.column3} />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rowLayout: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  column1: {
+    height: 50,
+    flex: 1,
+  },
+  column2: {
+    height: 50,
+    flex: 2,
+  },
+  column3: {
+    height: 50,
+    flex: 1,
+  },
+});
 
 export default AntStats;
