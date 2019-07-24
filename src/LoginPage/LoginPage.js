c18023194d7124bea345dd4e848b75f6b1cc397e
Derek Nowicki <derek@knowikiapps.com>
2019-07-23 19:52:47 -0700

    username and password fields and labels

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, SafeAreaView } from 'react-native';
+
 import PropTypes from 'prop-types';
 
 class LoginPage extends Component {
@@ -10,17 +11,49 @@ class LoginPage extends Component {
 
   componentDidMount() {}
 
-  componentDidUpdate(prevProps, prevState) {}
-
   render() {
-    return <View style={styles.rowLayout} />;
+    return (
+      <SafeAreaView style={styles.colLayout}>
+        <View style={styles.rowStyle}>
+          <Text style={styles.label}>Username</Text>
+        </View>
+        <View style={styles.rowStyle}>
+          <TextInput
+            onChangeText={text => this.setState({ username: text })}
+            value={this.state.username}
+            style={styles.field}
+          />
+        </View>
+        <View style={styles.rowStyle}>
+          <Text style={styles.label}>Password</Text>
+        </View>
+        <View style={styles.rowStyle}>
+          <TextInput
+            onChangeText={text => this.setState({ password: text })}
+            value={this.state.password}
+            style={styles.field}
+            secureTextEntry
+          />
+        </View>
+      </SafeAreaView>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  rowLayout: {
+  colLayout: {
     flex: 1,
-    flexDirection: 'row',
+  },
+  field: {
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  label: {
+    width: 100,
+    height: '100%',
+  },
+  rowStyle: {
+    flex: 1,
   },
 });
 
