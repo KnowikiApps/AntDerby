42715de9995cd99b9ce92d452f5bc6f793c605e1
Derek Nowicki <derek@knowikiapps.com>
2019-07-24 14:09:27 -0700

    add components for logo, welcome text, odds calculation

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import PropTypes from 'prop-types';
 
 class Header extends Component {
@@ -11,14 +11,52 @@ class Header extends Component {
   componentDidMount() {}
 
   render() {
-    return <View style={styles.rowLayout} />;
+    return (
+      <View style={styles.colLayout}>
+        <View style={styles.row1}>
+          <Image
+            source={require('../img/Logo.png')}
+            style={styles.antImage}
+            resizeMode="contain"
+          />
+        </View>
+        <View style={styles.row2}>
+          <Text>Welcome Dude!</Text>
+        </View>
+        <View style={styles.row3}>
+          <Text>Welcome Dude!</Text>
+          <Button
+            title="Calculate Odds"
+            onPress={() => {
+              console.log('calculate odds');
+            }}
+          />
+        </View>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  rowLayout: {
+  colLayout: {
+    height: '25%',
+    width: '100%',
+    backgroundColor: '#afa939',
+  },
+  row1: {
+    flex: 1,
+  },
+  row2: {
+    flex: 1,
+  },
+  row3: {
     flex: 1,
     flexDirection: 'row',
+  },
+  antImage: {
+    flex: 1,
+    height: undefined,
+    width: undefined,
   },
 });
 
