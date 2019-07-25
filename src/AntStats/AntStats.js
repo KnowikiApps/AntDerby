2710a99e3fb41d92a297cd95c081ce18ec02cc82
Derek Nowicki <derek@knowikiapps.com>
2019-07-25 10:06:58 -0700

    AntStats row styling

--- src/AntStats/AntStats.js
+++ src/AntStats/AntStats.js
@@ -4,41 +4,48 @@ import PropTypes from 'prop-types';
 
 function AntStats(props) {
   return (
-    <View style={styles.rowLayout}>
-      <View style={styles.column1}>
-        <Image
-          source={require('../img/Ant.png')}
-          style={[styles.antImage, { tintColor: props.color.toLowerCase() }]}
-          resizeMode="contain"
-          transform={[{ scaleX: '0.75' }, { scaleY: '0.75' }]}
-        />
-      </View>
-      <View style={styles.column2}>
-        <Text>{props.name}</Text>
-        <Text>{`color: ${props.color}`}</Text>
-        <View style={styles.rowLayout}>
-          <Text>{`length: ${props.length}`}</Text>
-          <Text>{`    weight: ${props.weight}`}</Text>
+    <View style={styles.colLayout}>
+      <View style={styles.rowSpace} />
+      <View style={styles.rowLayout}>
+        <View style={styles.column1}>
+          <Image
+            source={require('../img/Ant.png')}
+            style={[styles.antImage, { tintColor: props.color.toLowerCase() }]}
+            resizeMode="contain"
+            transform={[{ scaleX: '0.75' }, { scaleY: '0.75' }]}
+          />
+        </View>
+        <View style={styles.column2}>
+          <Text style={styles.name}>{props.name}</Text>
+          <Text>{`color: ${props.color}`}</Text>
+          <View style={styles.rowLayout}>
+            <Text>{`length: ${props.length}`}</Text>
+            <Text>{`    weight: ${props.weight}`}</Text>
+          </View>
+        </View>
+        <View style={styles.column3}>
+          <View>
+            <Text style={styles.headingText}>Odds</Text>
+            <Text style={styles.infoText}>{props.odds}</Text>
+          </View>
+          <View>
+            <Text style={styles.headingText}>Status</Text>
+            <Text style={styles.infoText}>{props.status}</Text>
+          </View>
         </View>
       </View>
-      <View style={styles.column3}>
-        <View>
-          <Text style={styles.headingText}>Odds</Text>
-          <Text style={styles.infoText}>{props.odds}</Text>
-        </View>
-        <View>
-          <Text style={styles.headingText}>Status</Text>
-          <Text style={styles.infoText}>{props.status}</Text>
-        </View>
-      </View>
+      <View style={styles.rowSpace} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   rowLayout: {
-    flex: 1,
+    flex: 2,
     flexDirection: 'row',
+  },
+  colLayout: {
+    flexDirection: 'column',
   },
   column1: {
     flex: 1,
@@ -61,6 +68,13 @@ const styles = StyleSheet.create({
   infoText: {
     textAlign: 'center',
     fontSize: 10,
+  },
+  name: {
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+  },
+  rowSpace: {
+    height: 10,
   },
 });
 
